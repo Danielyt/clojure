a119583fc62c0d5744f909c5f54e47fedac3338e
Daniel Rusev <danielytt_rusev@yahoo.com>
2013-12-01 17:35:16 +0200

    Update sandbox.clj

--- sandbox/sandbox.clj
+++ sandbox/sandbox.clj
@@ -1,2 +1,6 @@
-(defn factorial [n]
-  (reduce * (range 1 (inc n))))
+(def my-bag ["laptop" "knife" "earphones" "phone"])
+(def stuff-for-fmi ["clicker" "piece of mind" "Snickers"])
+
+(conj my-bag "ФЗФ file") ; ["laptop" "knife" "earphones" "phone" "ФЗФ file"]
+(into my-bag stuff-for-fmi) ; ["laptop" "knife" "earphones" "phone" "clicker" "piece of mind" "Snickers"]
+(my-bag 1) ; "knife" 
