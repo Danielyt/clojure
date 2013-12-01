8cae3aa9c4c7d6d9eb0fdff0dcc032c019f7a550
Daniel Rusev <danielytt_rusev@yahoo.com>
2013-12-01 17:29:00 +0200

    Update sandbox.clj

--- sandbox/sandbox.clj
+++ sandbox/sandbox.clj
@@ -1,9 +1,4 @@
-(def fib (fn [n]
-           (if (= n 0)
-             1
-             (if (= n 1)
-               1
-               (+ (fib (- n 1)) (fib (- n 2)))))))
+(defn factorial [n]
+  	(reduce * (range 1 (inc n))))
 
-(fib 0)  ; 1
-(fib 11) ; 144
+
