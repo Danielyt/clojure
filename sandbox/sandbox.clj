dfe584f9269fb2e69334e3a92ac8aa3b456b6386
Daniel Rusev <danielytt_rusev@yahoo.com>
2013-12-16 23:35:46 +0200

    Update sandbox.clj

--- sandbox/sandbox.clj
+++ sandbox/sandbox.clj
@@ -14,19 +14,16 @@
   (let [fib-seq (lazy-cat [1 1] (map + (rest fib-seq) fib-seq))]
     (nth fib-seq n)))
 
-(def account-1 (ref 100))
-(def account-2 (ref 100))
-
-(defn withdraw
+(defn safe-withdraw
   [account amount]
   (dosync
-    (when (>= (+ @account-1 @account-2) amount)
+    (when (>= (+ (ensure account-1) (ensure account-2)) amount)
       (Thread/sleep 100)
       (alter account - amount))))
 
-(dorun (pvalues (withdraw account-1 200)
-                (withdraw account-2 200)))
+(dorun (pvalues (safe-withdraw account-1 200)
+                (safe-withdraw account-2 200)))
 
-(println "Write skew:")
-(printf "- Account 1: %s\n" @account-1)	; - Account 1: -100
+(println "Using ensure:")
+(printf "- Account 1: %s\n" @account-1)	; - Account 1: 100
 (printf "- Account 2: %s\n" @account-2)	; - Account 2: -100
