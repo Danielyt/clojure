587684ed19d6df2e8d10b5115b11d2792ce06991
Daniel Rusev <danielytt_rusev@yahoo.com>
2013-12-16 22:54:34 +0200

    Update sandbox.clj

--- sandbox/sandbox.clj
+++ sandbox/sandbox.clj
@@ -14,8 +14,8 @@
   (let [fib-seq (lazy-cat [1 1] (map + (rest fib-seq) fib-seq))]
     (nth fib-seq n)))
 
-(def people #{"Clayton Shantelle" "Pauleen Linton" "Aniyah Deeann"})
-(conj people "Rosamund Maxwell")		; #{“Clayton Shantelle" “Pauleen Linton" "Aniyah Deeann“ “Rosamund Maxwell”}
-
-(conj people "Pauleen Linton")		; #{“Clayton Shantelle" “Pauleen Linton" "Aniyah Deeann"}
-(disj people "Pauleen Linton")		; #{“Clayton Shantelle“ “Aniyah Deeann"})
+(def primes #{13 11 7 5 3 2})
+(primes 11)		; 11
+(primes 4)		; nil
+(contains? primes 7)	; true
+(contains? primes 4)	; false
