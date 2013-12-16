bb453e3f89be1defeaa44f36795b93fc499fa184
Daniel Rusev <danielytt_rusev@yahoo.com>
2013-12-16 23:01:32 +0200

    Update sandbox.clj

--- sandbox/sandbox.clj
+++ sandbox/sandbox.clj
@@ -14,9 +14,17 @@
   (let [fib-seq (lazy-cat [1 1] (map + (rest fib-seq) fib-seq))]
     (nth fib-seq n)))
 
-(use 'clojure.set)
-(difference #{1 2 3 4} #{2 4 6 8})		; #{1 3}
-(intersection #{1 2 3} #{2 3 4 5})		; #{2 3}
-(subset? #{1 2} #{0 1 2 3})		; true
-(union #{0 1 2 3} #{2 3 4 5})		; #{0 1 2 3 4 5}
+(def numbers [1 2 3 4])
+(def squares
+  (for [n numbers] (* n n)))
+
+squares ; (1 4 9 16)
+
+(def pythagorean
+  (for   [x (range 1 15)
+          y (range 1 15)
+          z (range 1 15)
+          :when (and (< x y z)
+                     (= (+ (* x x) (* y y)) (* z z)))] [x y z]))
+pythagorean	; ([3 4 5] [5 12 13] [6 8 10])
 
